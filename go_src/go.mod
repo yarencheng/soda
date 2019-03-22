656f3df34834a14f8c1d9c77f81b058ea5c3d5c3
Aren Chen <yarencheng@gmail.com>
2019-03-22 00:41:35 -0700

    add POST /photo

--- go_src/go.mod
+++ go_src/go.mod
@@ -1,11 +1,17 @@
 module github.com/yarencheng/soda/go_src
 
 require (
+	github.com/0xAX/notificator v0.0.0-20181105090803-d81462e38c21 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
 	github.com/gin-gonic/gin v1.3.0
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/mattn/go-shellwords v1.0.5 // indirect
+	github.com/satori/go.uuid v1.2.0
 	github.com/ugorji/go/codec v0.0.0-20190320090025-2dc34c0b8780 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- go_src/main.go
+++ go_src/main.go
@@ -1,13 +1,91 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
+)
+
+var photoDir string
+
+type Photo struct {
+	ID          uuid.UUID
+	Title       string
+	Description string
+}
 
 func main() {
+	photoDir, exist := os.LookupEnv("PHOTO_DIR")
+	if !exist {
+		log.Fatalln("ENV PHOTO_DIR is not set")
+	}
+	log.Printf("photoDir = %v", photoDir)
+
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	r.POST("/photo", func(c *gin.Context) {
+		// single file
+		file, err := c.FormFile("file")
+		if err != nil {
+			log.Printf("err: %v", err)
+			c.JSON(500, gin.H{
+				"status": "failed",
+			})
+			return
+		}
+		log.Printf("file.Filename = %v", file.Filename)
+
+		photo := &Photo{
+			ID:          uuid.NewV4(),
+			Title:       "empty",
+			Description: "empty",
+		}
+		b, err := json.Marshal(photo)
+		if err != nil {
+			log.Printf("err: %v", err)
+			c.JSON(500, gin.H{
+				"status": "failed",
+			})
+			return
+		}
+
+		jsonData := string(b)
+		log.Printf("jsonData = %v", jsonData)
+
+		dir := photoDir + "/" + photo.ID.String() + "/"
+		log.Printf("dir = %v", dir)
+		err = os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			log.Printf("err: %v", err)
+			c.JSON(500, gin.H{
+				"status": "failed",
+			})
+			return
+		}
+
+		dst := dir + "/photo"
+		log.Printf("dst = %v", dst)
+		err = c.SaveUploadedFile(file, dst)
+		if err != nil {
+			log.Printf("err: %v", err)
+			c.AbortWithStatus(500)
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	r.Run()
 }
